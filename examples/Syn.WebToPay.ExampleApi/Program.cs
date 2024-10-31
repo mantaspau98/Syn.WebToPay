c8313ac20eff993285e8dfd701a09ecc054459b0
Mantas <mantas.pau@gmail.com>
2024-10-31 11:25:06 +0200

    examples in progress

--- examples/Syn.WebToPay.ExampleApi/Program.cs
+++ examples/Syn.WebToPay.ExampleApi/Program.cs
@@ -21,7 +21,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/initiatePayment", () =>
     {
         var forecast = Enumerable.Range(1, 5).Select(index =>
                 new WeatherForecast
@@ -33,8 +33,21 @@ app.MapGet("/weatherforecast", () =>
             .ToArray();
         return forecast;
     })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+    .WithName("PaymentInitiation");
+
+app.MapGet("/callback", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("PaymentCallback");
 
 app.Run();
 
